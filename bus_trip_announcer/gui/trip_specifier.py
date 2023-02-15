210f8f533c38ec3ef4416d55f3acd4b78da60455
joel woodfield <joelwoodfield@gmail.com>
2023-02-15 11:46:20 +0700

    Competed the GUI for trip viewer and coordinates specifier

---
+++ bus_trip_announcer/gui/trip_specifier.py
@@ -0,0 +1,108 @@
+import time
+from datetime import timedelta
+
+import flet as ft
+
+from bus_trip_announcer.database.finders import TripFinder, DirectionFinder
+from bus_trip_announcer.specifiers.trip_specifier import TripSpecifier
+from bus_trip_announcer.utils import Coordinates
+
+
+class GUITripSpecifier(TripSpecifier):
+
+    def __init__(self, direction_finder: DirectionFinder, trip_finder: TripFinder, page: ft.Page):
+        super().__init__(direction_finder, trip_finder)
+        self._page = page
+        self._fully_specified = False
+
+    def specify_route_number(self) -> None:
+        def btn_clicked(_):
+            self.trip_status.route_number = int(field.value)
+            self._page.controls.pop()
+            self.specify_direction()
+
+        field = ft.TextField(width=100)
+        self._page.add(
+            ft.Row(controls=[
+                ft.Text("Route Number"),
+                field,
+                ft.ElevatedButton(text="Enter", on_click=btn_clicked),
+            ])
+        )
+
+
+    def specify_direction(self) -> None:
+        def btn_i_clicked(i):
+            def btn_clicked(e):
+                direction = self._direction_finder.get_direction(
+                    self.trip_status.route_number, headsigns[i]
+                )
+                self.trip_status.direction = direction
+                self._page.controls.pop()
+                self.specify_coordinates()
+            return btn_clicked
+
+        headsigns = self._direction_finder.get_headsigns(
+            self.trip_status.route_number
+        )
+
+        prompt = ft.Text("Which Headsign?")
+
+        headsign_buttons = []
+        for i, headsign in enumerate(headsigns):
+            headsign_buttons.append(ft.FilledTonalButton(text=headsign,
+                                                      on_click=btn_i_clicked(i)))
+
+        self._page.add(ft.Column(controls=[prompt, *headsign_buttons]))
+
+    def specify_coordinates(self) -> None:
+        def btn_clicked(e):
+            self.trip_status.coordinates = Coordinates(
+                float(latitude.value), float(longitude.value)
+            )
+            self._page.controls.pop()
+            self.specify_time()
+
+        latitude = ft.TextField(width=150)
+        row1 = ft.Row(controls=[
+            ft.Text("Current Latitude"),
+            latitude,
+        ])
+
+        longitude = ft.TextField(width=150)
+        row2 = ft.Row(controls=[
+            ft.Text("Current Longitude"),
+            longitude,
+        ])
+
+        self._page.add(ft.Column(controls=[
+            row1,
+            row2,
+            ft.ElevatedButton("Enter", on_click=btn_clicked)
+        ]))
+
+    def specify_time(self) -> None:
+        def btn_clicked(e):
+            self._time = timedelta(hours=int(hours.value), minutes=int(minutes.value))
+            self._page.controls.pop()
+            self._fully_specified = True
+
+        prompt = ft.Text("Enter the time")
+        hours = ft.TextField(hint_text="HH", width=100)
+        minutes = ft.TextField(hint_text="MM", width=100)
+        inputs = ft.Row(controls=[
+            hours,
+            ft.Text(":"),
+            minutes
+        ])
+
+        self._page.add(ft.Column(controls=[
+            prompt,
+            inputs,
+            ft.ElevatedButton("Enter", on_click=btn_clicked)
+        ]))
+
+    def specify_all(self):
+        self.specify_route_number()
+        while not self._fully_specified:
+            time.sleep(1)
--- bus_trip_announcer/specifiers/trip_specifier.py
+++ bus_trip_announcer/specifiers/trip_specifier.py
@@ -2,7 +2,7 @@
 This module allows for the ability to specify the trip for the announcer
 to display the next stops for.
 """
-
+import time
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 
@@ -55,6 +55,27 @@ class TripSpecifier(ABC):
         """
         Specifies the time.
         """
+
+    def create_announcer(self) -> TripAnnouncer:
+        """
+        Creates an announcer with the fully-specified trip status and time.
+        """
+        stops_finder = NextStopsFinder(self._get_trip())
+        announcer = TripAnnouncer(stops_finder)
+        announcer.update_next_stops(self.trip_status)
+
+        return announcer
+
+    def _get_trip(self) -> Trip:
+        """
+        Returns the trip with the fully-specified trip status and time.
+        """
+        return self._trip_finder.get_trip(
+            self.trip_status.route_number,
+            self.trip_status.direction,
+            self.trip_status.coordinates,
+            self._time,
+        )
 
 
 class CommandLineTripSpecifier(TripSpecifier):
@@ -116,121 +137,7 @@ class CommandLineTripSpecifier(TripSpecifier):
         self.specify_coordinates()
         self.specify_time()
 
-    def _get_trip(self) -> Trip:
-        """
-        Returns the trip with the fully-specified trip status and time.
-        """
-        return self._trip_finder.get_trip(
-            self.trip_status.route_number,
-            self.trip_status.direction,
-            self.trip_status.coordinates,
-            self._time,
-        )
 
-    def create_announcer(self) -> TripAnnouncer:
-        """
-        Creates an announcer with the fully-specified trip status and time.
-        """
-        stops_finder = NextStopsFinder(self._get_trip())
-        announcer = TripAnnouncer(stops_finder)
-        announcer.update_next_stops(self.trip_status)
-
-        return announcer
-
-
-class GUITripSpecifier(TripSpecifier):
-
-    def __init__(self, direction_finder: DirectionFinder, trip_finder: TripFinder, page: ft.Page):
-        super().__init__(direction_finder, trip_finder)
-        self._page = page
-
-    def specify_route_number(self) -> None:
-        def btn_clicked(_):
-            self.trip_status.route_number = int(field.value)
-            self._page.controls.pop()
-            self.specify_direction()
-
-        field = ft.TextField(width=100)
-        self._page.add(
-            ft.Row(controls=[
-                ft.Text("Route Number"),
-                field,
-                ft.ElevatedButton(text="Enter", on_click=btn_clicked),
-            ])
-        )
-
-    def specify_direction(self) -> None:
-        def btn_i_clicked(i):
-            def btn_clicked(e):
-                direction = self._direction_finder.get_direction(
-                    self.trip_status.route_number, headsigns[i]
-                )
-                self.trip_status.direction = direction
-                self._page.controls.pop()
-                self.specify_coordinates()
-            return btn_clicked
-
-        headsigns = self._direction_finder.get_headsigns(
-            self.trip_status.route_number
-        )
-
-        prompt = ft.Text("Which Headsign?")
-
-        headsign_buttons = []
-        for i, headsign in enumerate(headsigns):
-            headsign_buttons.append(ft.FilledTonalButton(text=headsign,
-                                                      on_click=btn_i_clicked(i)))
-
-        self._page.add(ft.Column(controls=[prompt, *headsign_buttons]))
-
-    def specify_coordinates(self) -> None:
-        def btn_clicked(e):
-            self.trip_status.coordinates = Coordinates(
-                float(latitude.value), float(longitude.value)
-            )
-            self._page.controls.pop()
-            self.specify_time()
-
-        latitude = ft.TextField(width=150)
-        row1 = ft.Row(controls=[
-            ft.Text("Current Latitude"),
-            latitude,
-        ])
-
-        longitude = ft.TextField(width=150)
-        row2 = ft.Row(controls=[
-            ft.Text("Current Longitude"),
-            longitude,
-        ])
-
-        self._page.add(ft.Column(controls=[
-            row1,
-            row2,
-            ft.ElevatedButton("Enter", on_click=btn_clicked)
-        ]))
-
-    def specify_time(self) -> None:
-        def btn_clicked(e):
-            self._time = timedelta(hours=int(hours.value), minutes=int(minutes.value))
-            self._page.controls.pop()
-
-        prompt = ft.Text("Enter the time")
-        hours = ft.TextField(hint_text="HH", width=100)
-        minutes = ft.TextField(hint_text="MM", width=100)
-        inputs = ft.Row(controls=[
-            hours,
-            ft.Text(":"),
-            minutes
-        ])
-
-        self._page.add(ft.Column(controls=[
-            prompt,
-            inputs,
-            ft.ElevatedButton("Enter", on_click=btn_clicked)
-        ]))
-
-    def specify_all(self):
-        self.specify_route_number()
 
 
 class NoRouteNumberError(Exception):
