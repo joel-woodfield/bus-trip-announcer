ab4d3c834b812dfaf06dfd4babf6a7982a89786b
joel woodfield <joelwoodfield@gmail.com>
2023-02-15 09:51:21 +0700

    Added the GUI trip specifier
    - [ ] GUITripSpecifier class
    - [ ] LocationSpecifier uses 'specify' instead of 'update'
    - [ ] _time_until_stop_format is a interface method
    
    #60

--- bus_trip_announcer/specifiers/location_specifier.py
+++ bus_trip_announcer/specifiers/location_specifier.py
@@ -21,45 +21,49 @@ class LocationSpecifier(ABC):
         self._current_status = current_status
 
     @abstractmethod
-    def input_coordinates(self) -> None:
+    def specify_coordinates(self) -> None:
         """Specifies the coordinates to the announcer."""
 
-    @abstractmethod
-    def input_route_number(self) -> None:
-        """Specifies the route number to the announcer."""
-
-    @abstractmethod
-    def input_direction(self) -> None:
-        """Specifies the direction to the announcer."""
-
-    @abstractmethod
     def update_trip_announcer(self) -> None:
-        """Specifies the current trip status to the announcer."""
+        self.specify_coordinates()
+        self._trip_announcer.update_next_stops(self._current_status)
+
+    # @abstractmethod
+    # def input_route_number(self) -> None:
+    #     """Specifies the route number to the announcer."""
+    #
+    # @abstractmethod
+    # def input_direction(self) -> None:
+    #     """Specifies the direction to the announcer."""
+    #
+    # @abstractmethod
+    # def update_trip_announcer(self) -> None:
+    #     """Specifies the current trip status to the announcer."""
 
 
-class CommandlineLocationUpdator(LocationSpecifier):
+class CommandlineLocationSpecifier(LocationSpecifier):
     """
     Specifies the current trip status to the announcer by asking for
     input in the command line.
     """
+    #
+    # def input_route_number(self) -> None:
+    #     new_route_number = int(input("Input route number: "))
+    #
+    #     current = self._current_status
+    #     self._current_status = TripStatus(
+    #         new_route_number, current.direction, current.coordinates
+    #     )
+    #
+    # def input_direction(self) -> None:
+    #     new_direction = Direction[input("Input the direction: ")]
+    #
+    #     current = self._current_status
+    #     self._current_status = TripStatus(
+    #         current.route_number, new_direction, current.coordinates
+    #     )
 
-    def input_route_number(self) -> None:
-        new_route_number = int(input("Input route number: "))
-
-        current = self._current_status
-        self._current_status = TripStatus(
-            new_route_number, current.direction, current.coordinates
-        )
-
-    def input_direction(self) -> None:
-        new_direction = Direction[input("Input the direction: ")]
-
-        current = self._current_status
-        self._current_status = TripStatus(
-            current.route_number, new_direction, current.coordinates
-        )
-
-    def input_coordinates(self) -> None:
+    def specify_coordinates(self) -> None:
         latitude = input("Input Latitude: ")
         longitude = input("Input Longitude: ")
         new_coordinates = Coordinates(float(latitude), float(longitude))
@@ -69,6 +73,32 @@ class CommandlineLocationUpdator(LocationSpecifier):
             current.route_number, current.direction, new_coordinates
         )
 
-    def update_trip_announcer(self) -> None:
-        self.input_coordinates()
-        self._trip_announcer.update_next_stops(self._current_status)
+
+# class FileLocationSpecifier(LocationSpecifier):
+#
+#     LATITUDE_ROW = 0
+#     LONGITUDE_ROW = 1
+#
+#     def __init__(self, trip_announcer: TripAnnouncer, current_status: TripStatus, input_file_path: str):
+#         super().__init__(trip_announcer, current_status)
+#         self._input_file_path = input_file_path
+#         self._data = None
+#
+#     def _read_file(self):
+#         with open(self._input_file_path, 'r') as f:
+#             self._data = f.readlines()
+#
+#     def input_coordinates(self) -> None:
+#         self._read_file()
+#         latitude, longitude = 0, 0
+#         for line in self._data:
+#             if line.startswith("Latitude"):
+#                 _, latitude = line.split(": ")
+#             if line.startswith("Longitude"):
+#                 _, longitude = line.split(": ")
+#         new_coordinates = Coordinates(float(latitude), float(longitude))
+#
+#         current = self._current_status
+#         self._current_status = TripStatus(
+#             current.route_number, current.direction, new_coordinates
+#         )
--- bus_trip_announcer/specifiers/trip_specifier.py
+++ bus_trip_announcer/specifiers/trip_specifier.py
@@ -6,6 +6,8 @@ to display the next stops for.
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 
+import flet as ft
+
 from bus_trip_announcer.announcer import TripAnnouncer
 from bus_trip_announcer.database.finders import DirectionFinder, TripFinder
 from bus_trip_announcer.models import Trip, TripStatus
@@ -134,6 +136,101 @@ class CommandLineTripSpecifier(TripSpecifier):
         announcer.update_next_stops(self.trip_status)
 
         return announcer
+
+
+class GUITripSpecifier(TripSpecifier):
+
+    def __init__(self, direction_finder: DirectionFinder, trip_finder: TripFinder, page: ft.Page):
+        super().__init__(direction_finder, trip_finder)
+        self._page = page
+
+    def specify_route_number(self) -> None:
+        def btn_clicked(_):
+            self.trip_status.route_number = int(field.value)
+            self._page.controls.pop()
+            self.specify_direction()
+
+        field = ft.TextField(width=100)
+        self._page.add(
+            ft.Row(controls=[
+                ft.Text("Route Number"),
+                field,
+                ft.ElevatedButton(text="Enter", on_click=btn_clicked),
+            ])
+        )
+
+    def specify_direction(self) -> None:
+        def btn_i_clicked(i):
+            def btn_clicked(e):
+                direction = self._direction_finder.get_direction(
+                    self.trip_status.route_number, headsigns[i]
+                )
+                self.trip_status.direction = direction
+                self._page.controls.pop()
+                self.specify_coordinates()
+            return btn_clicked
+
+        headsigns = self._direction_finder.get_headsigns(
+            self.trip_status.route_number
+        )
+
+        prompt = ft.Text("Which Headsign?")
+
+        headsign_buttons = []
+        for i, headsign in enumerate(headsigns):
+            headsign_buttons.append(ft.FilledTonalButton(text=headsign,
+                                                      on_click=btn_i_clicked(i)))
+
+        self._page.add(ft.Column(controls=[prompt, *headsign_buttons]))
+
+    def specify_coordinates(self) -> None:
+        def btn_clicked(e):
+            self.trip_status.coordinates = Coordinates(
+                float(latitude.value), float(longitude.value)
+            )
+            self._page.controls.pop()
+            self.specify_time()
+
+        latitude = ft.TextField(width=150)
+        row1 = ft.Row(controls=[
+            ft.Text("Current Latitude"),
+            latitude,
+        ])
+
+        longitude = ft.TextField(width=150)
+        row2 = ft.Row(controls=[
+            ft.Text("Current Longitude"),
+            longitude,
+        ])
+
+        self._page.add(ft.Column(controls=[
+            row1,
+            row2,
+            ft.ElevatedButton("Enter", on_click=btn_clicked)
+        ]))
+
+    def specify_time(self) -> None:
+        def btn_clicked(e):
+            self._time = timedelta(hours=int(hours.value), minutes=int(minutes.value))
+            self._page.controls.pop()
+
+        prompt = ft.Text("Enter the time")
+        hours = ft.TextField(hint_text="HH", width=100)
+        minutes = ft.TextField(hint_text="MM", width=100)
+        inputs = ft.Row(controls=[
+            hours,
+            ft.Text(":"),
+            minutes
+        ])
+
+        self._page.add(ft.Column(controls=[
+            prompt,
+            inputs,
+            ft.ElevatedButton("Enter", on_click=btn_clicked)
+        ]))
+
+    def specify_all(self):
+        self.specify_route_number()
 
 
 class NoRouteNumberError(Exception):
