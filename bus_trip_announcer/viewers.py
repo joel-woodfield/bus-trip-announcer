1b79ddb93c6e10e303fb8c3bc0b0dc0396df31f4
joel <joelwoodfield@gmail.com>
2023-03-05 11:52:15 +1000

    Made the viewer and location updator separate.
    - [ ] FletTripViewer class
    - [ ] LocationInput has update_coordianates instead of input, to distinguish from TripInput's input_coordinates
    - [ ] TripInput no longer relies on LocationInput
    - [ ] announcer's attributes are now all public
    - [ ] InputDevice has an announcer public attribute
    
    #70

--- bus_trip_announcer/viewers.py
+++ bus_trip_announcer/viewers.py
@@ -5,7 +5,11 @@ Contains the classes that display the trip information.
 import datetime
 from typing import Protocol
 
+import flet as ft
+
 from bus_trip_announcer.announcer import TripAnnouncer
+from bus_trip_announcer.models import TripStatus
+from bus_trip_announcer.utils import Coordinates
 
 
 class TripViewer(Protocol):
@@ -31,7 +35,7 @@ class TripViewer(Protocol):
         return f"{int(hours)}hr {int(minutes)}min"
 
 
-class CommandlineDisplay(TripViewer):
+class CommandLineTripViewer(TripViewer):
     """
     Displays the next stop on the commandline.
     """
@@ -53,8 +57,52 @@ class CommandlineDisplay(TripViewer):
         )
 
         header_length = max(len(line) for line in stops_display.split("\n"))
-        header = f"Route {self._trip_announcer.route_number}".center(
+        header = f"Route {self._trip_announcer.trip_status.route_number}".center(
             header_length, "-"
         )
         print("\n" + header)
         print(stops_display + "\n")
+
+
+class FletTripViewer(TripViewer):
+    NUM_STOPS_DISPLAYED = 5
+
+    def __init__(
+        self,
+        announcer: TripAnnouncer,
+        page: ft.Page,
+    ):
+        self._trip_announcer = announcer
+        self._page = page
+
+        if self._page.controls:
+            self._page.controls.pop()
+
+    def show_next_stops(self) -> None:
+        if len(self._page.controls) > 1:
+            self._page.controls.pop()
+        next_stops = self._trip_announcer.next_stops
+
+        stop_names = [stop.name for stop in next_stops]
+        stop_times = [
+            self._time_until_stop_format(stop.time_until_stop)
+            for stop in next_stops
+        ]
+
+        information = ft.Row(
+            controls=[
+                ft.Column(controls=[ft.Text(name) for name in stop_names]),
+                ft.Column(controls=[ft.Text(time) for time in stop_times]),
+            ]
+        )
+
+        self._page.add(
+            ft.Column(
+                controls=[
+                    ft.Text("Next Stops"),
+                    information,
+                ]
+            )
+        )
+
+
